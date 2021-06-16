198c6e043c749584ceb936111e395b76eedf940a
KKaciusis <kkaciusis@yahoo.com>
2021-06-16 17:46:34 +0200

    squere root

---
+++ 2/script.js
@@ -0,0 +1,8 @@
+"use strict"
+
+document.getElementById('Check').addEventListener('click', () => {
+
+    let input = document.getElementById("input").value;
+    let sqrt = Math.sqrt(input);
+    console.log(sqrt);
+})
