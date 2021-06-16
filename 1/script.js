45943e6908197880be47299b25338762ebca37b3
KKaciusis <kkaciusis@yahoo.com>
2021-06-16 18:18:06 +0200

    2nd task

--- 1/script.js
+++ 1/script.js
@@ -1,7 +1,30 @@
 "use strict"
-let masyvas = []
 
-function addvalue(array) {
-    array[array.length] = document.getElementById("input").value;
-    console.log(array)
+let masyvas = [];
+
+function addValue() {
+    masyvas.push(document.getElementById("input").value);
+    // masyvas[masyvas.length] = document.getElementById("input").value;
 }
+
+function removeValue() {
+    masyvas.pop();
+}
+
+function showResult() {
+    console.log(masyvas);
+}
+
+document.getElementById('check').addEventListener('click', () => {
+    addValue();
+});
+
+document.getElementById('remove').addEventListener('click', () => {
+    removeValue();
+});
+
+document.getElementById('show').addEventListener('click', () => {
+    showValue();
+});
+
+// document.getElementById('check').addEventListener('click', () => console.log([document.getElementById("input").value]));
